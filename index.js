8bd35d767e3f36b93ae5addbe2787149b8ffb44f
hanselcarter <barahonahansel@hotmail.com>
2020-03-04 03:43:44 -0600

    adding user schema

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 //import routes
 const authRoute = require("./routes/auth");
-
+//connect to fb
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("connected to db")
+);
+//middleware
+app.use(express.json());
 //routes middleware
 app.use("/api/user", authRoute);
 
