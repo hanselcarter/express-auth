af89e9cc3501f6cf97d8c31344d9d83e71883bb3
hanselcarter <barahonahansel@hotmail.com>
2020-03-04 02:57:18 -0600

    setting things up

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+//import routes
+const authRoute = require("./routes/auth");
+
+//routes middleware
+app.use("/api/user", authRoute);
+
+app.listen(3000, () => console.log("server running"));
