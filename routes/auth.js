454095fa1725a687f2f696a49b2d577a889d33a4
hanselcarter <barahonahansel@hotmail.com>
2020-03-04 11:55:30 -0600

    validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+  //validate data before saving it
+  const { error } = registerValidation(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
